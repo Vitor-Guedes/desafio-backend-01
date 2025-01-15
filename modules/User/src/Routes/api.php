ed15e71aa5b4c47dd62d4f0b356a35d1d7ad80c1
Vitor Guedesw <vtrf@gmail.com>
2025-01-15 17:18:47 -0300

    env test, github act

--- modules/User/src/Routes/api.php
+++ modules/User/src/Routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserController::class)->group(function () {
     Route::post('transfer', 'transfer')->name('api.transfer');
 });
+
+Route::prefix('tests')->group(function () {
+    Route::get('/api/v2/authorize', function () {
+        return response([
+            'status' => 'success',
+            'data' => [
+                'authorization' => true
+            ]
+        ], 200);
+    })->name('test.extenal-service.authorize');
+});
